ef7a8216f642a7c729152b155801303692c4e2aa
Ashish Patel <ashishsushilpatel@gmail.com>
2019-12-10 02:06:48 +0530

    feat(100-algorithms): add solutions till problem 30

---
+++ 100-algorithms/030-common-character-count.js
@@ -0,0 +1,15 @@
+/**
+ * 
+ * Ashish Patel
+ * e: ashishsushilPatel@gmail.com
+ * w: https://ashish.me
+ *
+ */
+
+function commonCharacterCount(value) {
+  return value
+}
+
+test('common Character Count', () => {
+  expect(commonCharacterCount('Ashish')).toEqual('Ashish')
+});
