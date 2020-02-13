cb6e1376b8053b1f8bba859fe70f18ea9ec226b5
Ashish Patel <ashishsushilpatel@gmail.com>
2020-02-13 20:52:46 +0000

    feat(leetcode): add question for problem 53

--- leetcode/053-maximum-subarray.js
+++ leetcode/053-maximum-subarray.js
@@ -6,6 +6,16 @@
  *
  */
 
+/* 
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+*/
+
 function maximumSubarray(nums) {
   let sum = nums[0]
   for (let index = 1; index < nums.length; index++) {
@@ -15,8 +25,6 @@ function maximumSubarray(nums) {
   return sum
 }
 
-console.log(maximumSubarray([-2,1,-3,4,-1,2,1,-5,4]))
-
-// test('maximum Subarray', () => {
-//   expect(maximumSubarray([-2,1,-3,4,-1,2,1,-5,4])).toEqual(6)
-// });
+test('maximum Subarray', () => {
+  expect(maximumSubarray([-2,1,-3,4,-1,2,1,-5,4])).toEqual(6)
+});
