30190f42ae70f66f9184627bae7b35fbda5de209
Ashish Patel <ashishsushilpatel@gmail.com>
2020-02-23 08:56:10 +0000

    feat(leetcode): add solution for problem 3

---
+++ leetcode/152-maximum-product-subarray.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * Ashish Patel
+ * e: ashishsushilPatel@gmail.com
+ * w: https://ashish.me
+ *
+ */
+
+/* 
+  Given an integer array nums, find the contiguous subarray within an array (containing at least one number) which has the largest product.
+
+  Example 1:
+
+  Input: [2,3,-2,4]
+  Output: 6
+  Explanation: [2,3] has the largest product 6.
+  Example 2:
+
+  Input: [-2,0,-1]
+  Output: 0
+  Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+*/
+
+function maximumProductSubarray(value) {
+  return value
+}
+
+test('maximum Product Subarray', () => {
+  expect(maximumProductSubarray('Ashish')).toEqual('Ashish')
+});
