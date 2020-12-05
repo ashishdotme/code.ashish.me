302baceac499b8c4368b60d9b8efec6a6d5eb1df
Ashish Patel <ashishsushilpatel@gmail.com>
2020-12-05 20:35:54 +0530

    feat: add question for problem 617

--- leetcode/617-merge-two-binary-trees.js
+++ leetcode/617-merge-two-binary-trees.js
@@ -6,6 +6,15 @@
  *
  */
 
+/*
+ * Given two binary trees and imagine that when you put one of them to cover the other,
+ * some nodes of the two trees are overlapped while the others are not.
+ *
+ * You need to merge them into a new binary tree. The merge rule is that if two nodes
+ * overlap, then sum node values up as the new value of the merged node. Otherwise,
+ * the NOT null node will be used as the node of new tree.
+ */
+
 function TreeNode(val) {
   this.val = val
   this.left = this.right = null
