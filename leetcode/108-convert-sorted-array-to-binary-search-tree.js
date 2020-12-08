7b78f7bd201fc84d4c6e6b52d9dacfbfd76258ea
Ashish Patel <ashishsushilpatel@gmail.com>
2020-12-09 01:39:28 +0530

    feat: add solution for leetcode problem 108

--- leetcode/108-convert-sorted-array-to-binary-search-tree.js
+++ leetcode/108-convert-sorted-array-to-binary-search-tree.js
@@ -11,10 +11,19 @@ function TreeNode(val) {
   this.left = this.right = null
 }
 
-const convertSortedArrayToBinarySearchTree = (val) => {
-  return val
+const convertSortedArrayToBinarySearchTree = treeArray => {
+  const buildTree = (treeArray, left, right) => {
+    if (left > right) {
+      return null
+    }
+    const mid = Math.floor(left + (right - left) / 2)
+    const root = new TreeNode(treeArray[mid])
+    root.left = buildTree(treeArray, left, mid - 1)
+    root.right = buildTree(treeArray, mid + 1, right)
+    return root
+  }
+  const tree = buildTree(treeArray, 0, treeArray.length - 1)
+  return tree
 }
 
-test('convertSortedArrayToBinarySearchTree', () => {
-  expect(convertSortedArrayToBinarySearchTree([-10, -3, 0, 5, 9]])).toEqual(5)
-});
+console.log(convertSortedArrayToBinarySearchTree([-10, -3, 0, 5, 9]))
