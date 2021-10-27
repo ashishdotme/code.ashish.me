7d77202c1a1438f746273968b6475f6b5a7eb015
Ashish Patel <ashishsushilpatel@gmail.com>
2021-10-27 13:06:06 +0100

    feat: add solution for move zeros to end problem

--- geeksforgeeks/array/MoveZerosToEnd.java
+++ geeksforgeeks/array/MoveZerosToEnd.java
@@ -12,10 +12,11 @@ class MoveZerosToEnd {
     int count = 0;
     for(int i = 0; i < nums.length; i++){
       if(nums[i] != 0){
+        int temp = nums[count];
         nums[count] = nums[i];
+        nums[i] = temp;
         count += 1;
       }
-
     }
     return nums;
   }
