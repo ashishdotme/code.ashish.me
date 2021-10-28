5cc4055ed195c79a770003853964762f92398d38
Ashish Patel <ashishsushilpatel@gmail.com>
2021-10-28 17:32:32 +0100

    feat(geeksforgeeks): add trapping rain problem

---
+++ geeksforgeeks/array/TrappingRainWater.java
@@ -0,0 +1,32 @@
+/**
+ *
+ * Ashish Patel
+ * e: ashishsushilPatel@gmail.com
+ * w: https://ashish.me
+ *
+ */
+
+class TrappingRainWater {
+
+  static int func(int[] nums){
+    int result = 0;
+    for(int i = 1; i < nums.length - 1; i++){
+      int lMax = nums[0];
+      for(int j = 0; j < i; j++){
+        lMax = Math.max(lMax, nums[j]);
+      }
+      int rMax = nums[nums.length - 1];
+      for(int j = nums.length - 1; j > i; j--){
+        rMax = Math.max(rMax, nums[j]);
+      }
+      result += Math.min(rMax, lMax) - nums[i];
+    }
+    return result;
+  }
+
+  public static void main(String[] args){
+    int[] nums = {3, 0 , 1, 2, 5};
+    int result = func(nums);
+    System.out.println(result);
+  }
+}
