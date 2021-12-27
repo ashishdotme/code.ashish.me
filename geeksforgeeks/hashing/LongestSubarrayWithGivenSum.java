b404b784b2df5cdb6ace16316f95d0fc800b226b
Ashish Patel <ashishsushilpatel@gmail.com>
2021-12-27 01:07:10 +0000

    feat(gfg): add sol for longest subarray with sum

---
+++ geeksforgeeks/hashing/LongestSubarrayWithGivenSum.java
@@ -0,0 +1,39 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ *
+ * Ashish Patel
+ * e: ashishsushilPatel@gmail.com
+ * w: https://ashish.me
+ *
+ */
+
+class LongestSubarrayWithGivenSum {
+
+  static int func(int[] nums, int target){
+    int prefixSum = 0;
+    int res = 0;
+    HashMap<Integer, Integer> hs = new HashMap<>();
+    for(int i = 0; i < nums.length; i++){
+      prefixSum += nums[i];
+      if(prefixSum == target){
+        res = i + 1;
+      }
+      if(!hs.containsKey(prefixSum)){
+        hs.put(prefixSum, i);
+      }
+      if(hs.containsKey(prefixSum - target)){
+        res = Math.max(res, i - hs.get(prefixSum-target));
+      }
+    }
+    return res;
+  }
+
+  public static void main(String[] args){
+    int arr[] = new int[]{8, 3, 7};
+    int sum = 15;
+    int result = func(arr, sum);
+    System.out.println(result);
+  }
+}
