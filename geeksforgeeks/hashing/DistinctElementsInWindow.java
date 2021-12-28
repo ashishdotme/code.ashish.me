cd3153aafd763eec93b830b9882f590a7fe2e961
Ashish Patel <ashishsushilpatel@gmail.com>
2021-12-28 19:51:08 +0000

    feat(gfg): add solution for distinct ele in window

---
+++ geeksforgeeks/hashing/DistinctElementsInWindow.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+
+/**
+ *
+ * Ashish Patel
+ * e: ashishsushilPatel@gmail.com
+ * w: https://ashish.me
+ *
+ */
+
+class DistinctElementsInWindow {
+
+  static void printDistinct(int nums[], int k) {
+    HashMap<Integer, Integer> hm = new HashMap<>();
+    int result = 0;
+    for(int i = 0; i < k ; i++){
+      if(hm.containsKey(nums[i])){
+        hm.put(nums[i], hm.get(nums[i]) + 1);
+      } else {
+        result++;
+        hm.put(nums[i], 1);
+      }
+    }
+    System.out.println("Distinct Elements - " + hm.size());
+    for(int i = k; i < nums.length; i++){
+      if(hm.containsKey(nums[i-k])){
+        hm.put(nums[i-k], hm.get(nums[i-k]) - 1);
+      }
+      if(hm.get(nums[i-k]) == 0){
+        hm.remove(nums[i-k]);
+      }
+      if(hm.containsKey(nums[i])){
+        hm.put(nums[i], hm.get(nums[i]) + 1);
+      } else {
+        hm.put(nums[i], 0);
+      }
+      System.out.println("DistinctElements - " + hm.size());
+    }
+  }
+
+  public static void main(String[] args){
+    int arr[] = new int[]{10, 10, 5, 3, 20, 5};
+    int k=4;
+    printDistinct(arr, k);
+  }
+}
