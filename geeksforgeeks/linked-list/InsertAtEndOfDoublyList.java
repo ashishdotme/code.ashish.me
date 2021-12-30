78d76fae0dae35e7a4134519cf9b9f0d7addd7f4
Ashish Patel <ashishsushilpatel@gmail.com>
2021-12-30 10:56:07 +0000

    feat(gfg): add sol for end of doubly list

---
+++ geeksforgeeks/linked-list/InsertAtEndOfDoublyList.java
@@ -0,0 +1,45 @@
+/**
+ *
+ * Ashish Patel
+ * e: ashishsushilPatel@gmail.com
+ * w: https://ashish.me
+ *
+ */
+
+class InsertAtEndOfDoublyList {
+
+  static DoublyNode insertEnd(DoublyNode head, int data) {
+    DoublyNode newNode = new DoublyNode(data);
+    if(head == null){
+      return newNode;
+    }
+    DoublyNode current = head;
+    while(current.next != null){
+      current = current.next;
+    }
+    current.next = newNode;
+    newNode.prev = current;
+    return head;
+  }
+
+  public static void main(String[] args) {
+    DoublyNode head = new DoublyNode(10);
+    DoublyNode temp1 = new DoublyNode(20);
+    DoublyNode temp2 = new DoublyNode(30);
+    head.next = temp1;
+    temp1.prev = head;
+    temp1.next = temp2;
+    temp2.prev = temp1;
+    head = insertEnd(head, 40);
+    printlist(head);
+  }
+
+  public static void printlist(DoublyNode head) {
+    DoublyNode curr = head;
+    while (curr != null) {
+      System.out.print(curr.data + " ");
+      curr = curr.next;
+    }
+    System.out.println();
+  }
+}
