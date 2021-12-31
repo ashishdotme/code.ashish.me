a7f8decdae849879ac73e1c7f611fbcd28a8305c
Ashish Patel <ashishsushilpatel@gmail.com>
2021-12-31 09:25:36 +0000

    feat(cses): add sol for repeatitions problem

--- cses/introductory/Repeatitions.java
+++ cses/introductory/Repeatitions.java
@@ -1,3 +1,5 @@
+import java.util.*;
+import java.io.*;
 /**
  *
  * Ashish Patel
@@ -8,12 +10,37 @@
 
 class Repeatitions {
 
-  static int func(int num){
-    return num;
+
+  static PrintWriter pw;
+  static BufferedReader br;
+  static StringTokenizer st;
+
+  static long nl() {
+    return Long.parseLong(st.nextToken());
   }
 
-  public static void main(String[] args){
-    int result = func(4);
-    System.out.println(result);
+  static int ni() {
+    return Integer.parseInt(st.nextToken());
+  }
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    pw = new PrintWriter(System.out);
+    String x = br.readLine();
+    int count = 0;
+    int result = 1;
+    char l = 'A';
+    for (int i = 0; i < x.length(); ++i) {
+      if(x.charAt(i) == l){
+        ++count;
+        result = Math.max(result, count);
+      } else {
+        l = x.charAt(i);
+        count = 1;
+      }
+    }
+    pw.print(result);
+    br.close();
+    pw.close();
   }
 }
