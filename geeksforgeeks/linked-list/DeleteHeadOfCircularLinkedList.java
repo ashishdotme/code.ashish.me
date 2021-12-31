17a15a4c5b558ce05db3a6433c7b84c95c435a1c
Ashish Patel <ashishsushilpatel@gmail.com>
2021-12-31 00:36:08 +0000

    feat(gfg): add sol for del head of circular list

---
+++ geeksforgeeks/linked-list/DeleteHeadOfCircularLinkedList.java
@@ -0,0 +1,41 @@
+/**
+ *
+ * Ashish Patel
+ * e: ashishsushilPatel@gmail.com
+ * w: https://ashish.me
+ *
+ */
+
+class DeleteHeadOfCircularLinkedList {
+
+  static Node delHead(Node head){
+    if(head == null){
+      return null;
+    }
+    Node current = head;
+    while(current.next != head){
+      current = current.next;
+    }
+    current.next = head.next;
+    return current.next;
+  }
+
+  public static void main(String[] args){
+    Node head=new Node(10);
+    head.next=new Node(20);
+    head.next.next=new Node(30);
+    head.next.next.next=new Node(40);
+    head.next.next.next.next=head;
+    head=delHead(head);
+    printlist(head);
+  }
+
+  public static void printlist(Node head){
+    if(head==null)return;
+    Node r=head;
+    do{
+        System.out.print(r.data+" ");
+        r=r.next;
+    }while(r!=head);
+}
+}
