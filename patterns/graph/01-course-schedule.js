ba6c93d07b1ea2ab6bf3ba6d916524f0a644a30d
Ashish Patel <ashishsushilpatel@gmail.com>
2022-02-09 17:22:10 +0000

    feat(patterns): course schedule

--- patterns/graph/01-course-schedule.js
+++ patterns/graph/01-course-schedule.js
@@ -8,28 +8,34 @@
 
 const courseSchedule = (numCourses, prerequisites) => {
   let map = {}
-  for (let i = 0; i < numCourses; i++) {
-    map[i] = []
-  }
-  for (let i = 0; i < prerequisites.length; i++) {
-    map[i] = prerequisites[i]
+  const adj = [...Array(numCourses)].map(r => [])
+  for (let [u, v] of prerequisites) {
+    adj[v].push(u)
   }
   let visitedSet = new Set()
-  const dfs = () => {
-    if (visitedSet.has()) {
+  const dfs = e => {
+    if (visitedSet.has(e)) {
+      return false
     }
+    if (adj[e].length == 0) {
+      return true
+    }
+    visitedSet.add(e)
+    for (const c of adj[e]) {
+      if (!dfs(c)) {
+        return false
+      }
+    }
+    visitedSet.delete(e)
+    adj[e] = []
+    return true
   }
   for (let i = 0; i < numCourses; i++) {
-    if (!dfs(map[i])) {
+    if (!dfs(i)) {
       return false
     }
   }
   return true
 }
 
-console.log(
-  courseSchedule(2, [
-    [1, 0],
-    [0, 1],
-  ]),
-)
+console.log(courseSchedule(2, [[0, 1]]))
